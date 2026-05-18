3ea2c8a976d125eadcf0c6614823f0a0cc84a467
Nito <tanhoangwibu@gmail.com>
2026-05-18 21:38:55 +0700

    update

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_SETTINGS_MODULE', 'QL_tour.settings') # Thay bằng đường dẫn settings của bạn
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QL_tour.settings') # Thay bằng đường dẫn settings của bạn
 django.setup()
 
 from django.contrib.auth import get_user_model
