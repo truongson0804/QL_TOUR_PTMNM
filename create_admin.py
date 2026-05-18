622112eebc3448847f3e284e5121653f46619856
Nito <tanhoangwibu@gmail.com>
2026-05-18 21:35:15 +0700

    update: create superuser logic

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_SETTINGS_MODULE', 'tên_thư_mục_chứa_settings.settings') # Thay bằng đường dẫn settings của bạn
+os.environ.setdefault('DJANGO_SETTINGS_SETTINGS_MODULE', 'QL_tour.settings') # Thay bằng đường dẫn settings của bạn
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -10,8 +10,8 @@ User = get_user_model()
 
 # Lấy thông tin Admin từ biến môi trường (Bảo mật, không lo lộ pass trên GitHub)
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'MậtKhẩuAdminCủaBạn123')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'TanMeo2005')
 
 if not User.objects.filter(username=username).exists():
     print(f"Đang tạo tài khoản siêu admin: {username}")
